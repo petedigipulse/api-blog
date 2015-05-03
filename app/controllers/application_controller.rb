3fb14f6b10442f52c064e4645d8c9cf9b69a5486
petedigipulse <peter@digitalpulse.co.nz>
2015-05-03 14:26:40 +1200

    Fixed authenticate token for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,18 @@ class ApplicationController < ActionController::API
 	before_filter :authenticate_user_from_token, except: [:token]
 
 
-	def default_serializer_options
-		{ root: false }
-	end
+	# def default_serializer_options
+	# 	{ root: false }
+	# end
 
 	def token
 			authenticate_with_http_basic do |email, password|
+
 				user = User.find_by(email: email)
 				if user && user.password == password
 					render json: {token: user.auth_token}
 				else
-					render json: { error: 'Incorrect Credentials'}, staus: 401
+					render json: { error: 'Incorrect Credentials'}, status: 401
 				end
 			end
 	end
@@ -26,7 +27,7 @@ class ApplicationController < ActionController::API
 
 	def authenticate_user_from_token
 		unless authenticate_with_http_token { | token, options| User.find_by(auth_token: token)}
-			render json: { error: 'Bad Token'} status: 401
+			render json: { error: 'Bad Token'}, status: 401
 		end
 	end
 end
